6dc3e3a9b807dc486288680820a09c6ec2bf282a
Vishwajeet <kumbharevishwajeet@gmail.com>
2026-06-13 17:20:23 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,11 @@
+let btn1 = document.querySelector('#btn1');
+
+btn1.addEventListener("click", () => {
+    console.log("button click");
+});
+
+btn1.addEventListener("click", () => {
+    console.log("button click1");
+});
+
+let div = document.querySelector("div");

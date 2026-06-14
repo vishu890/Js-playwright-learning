6028b367d20f204bb8c9b36562c8af6b2a1f01d6
Vishwajeet <kumbharevishwajeet@gmail.com>
2026-06-14 18:22:10 +0530

    eventListner2

---
+++ eventlistener2.js
@@ -0,0 +1,17 @@
+let btn1 = document.querySelector('#btn1');
+
+btn1.addEventListener("click", () => {
+    console.log("button click");
+});
+
+btn1.addEventListener("click", () => {
+    console.log("button click1");
+});
+
+btn1.addEventListener("click", () => {
+    console.log("button click2");
+});
+
+
+
+let div = document.querySelector("div");
